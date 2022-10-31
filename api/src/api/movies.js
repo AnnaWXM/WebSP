d421a1e1be7eec8e5bcb0d0f38ecc72a078e4a83
AnnaWXM <xiaomininfi@gmail.com>
2022-10-31 07:55:41 +0200

    failed delete

--- api/src/api/movies.js
+++ api/src/api/movies.js
@@ -1,7 +1,57 @@
 
 const express = require('express');
 const router = express.Router();
+const movies = [
+  {
+    id: 1,
+    name: 'Pulp Fiction',
+  },
+  {
+    id: 2,
+    name: 'Iron Man',
+  },
+  {
+    id: 3,
+    name: 'Skyfall',
+  },
+];
 router.get('/', (req, res) => {
-  res.status(200).json([]);
+  res.status(200).json(movies);
 });
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;    //destructuring
+  const movie = movies.find((m) => m.id === Number(id));
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(404).json({ message: 'Not found' });
+  }
+});
+
+router.post('/', (req, res) => {
+  const { id, name } = req.body;
+  movies.push({ id, name });
+  res.status(201).json({ message: 'Created' });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const index = movies.findIndex((m) => m.id === Number(id));
+  const updatedMovie = {
+    id: Number(id),
+    name,
+  };
+  movies[index] = updatedMovie;
+  res.status(200).json({ message: 'Updated' });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  movies = movies.filter((m) => m.id !== Number(id));
+  res.status(200).json({ message: 'Deleted' });
+});
+
+
 module.exports = router;
--- api/test/api.test.js
+++ api/test/api.test.js
@@ -33,27 +33,103 @@ describe('Endpoint /api/v1/movies', () => {
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
-});
 
+  it('should return a list of movies when called with GET', (done) => {
+    const expected = [
+      {
+        id: 1,
+        name: 'Pulp Fiction',
+      },
+      {
+        id: 2,
+        name: 'Iron Man',
+      },
+      {
+        id: 3,
+        name: 'Skyfall',
+      },
+    ];
+    request(app)
+      .get('/api/v1/movies')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
 
-it('should return a list of movies when called with GET', (done) => {
-  const expected = [
-    {
-      id: 1,
-      name: 'Pulp Fiction',
-    },
-    {
+  it('should return the movies when called with GET id', (done) => {
+    const expected = {
       id: 2,
       name: 'Iron Man',
-    },
-    {
+    };
+    request(app)
+      .get('/api/v1/movies/2')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, expected, done);
+  });
+
+  it('should return 404 if nothing was found with the id', (done) => {
+    request(app)
+      .get('/api/v1/movies/20')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, { message: 'Not found' }, done);
+  });
+
+  it('should return 201 when new movie was added', async () => {
+    await request(app)
+      .post('/api/v1/movies/')
+      .set('Accept', 'application/json')
+      .send({ id: 4, name: 'Spiderman' })
+      .expect('Content-Type', /json/)
+      .expect(201, { message: 'Created' });
+    // Check that it was actually added as well
+    const expected = {
+      id: 4,
+      name: 'Spiderman',
+    };
+    await request(app)
+      .get('/api/v1/movies/4')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, expected);
+  });
+
+  it('should return 200 when movie was updated', async () => {
+    await request(app)
+      .patch('/api/v1/movies/3')
+      .set('Accept', 'application/json')
+      .send({ name: 'Casino Royale' })
+      .expect('Content-Type', /json/)
+      .expect(200, { message: 'Updated' });
+    // Check that it was actually added as well
+    const expected = {
       id: 3,
-      name: 'Skyfall',
-    },
-  ];
-  request(app)
-    .get('/api/v1/movies')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, done);
+      name: 'Casino Royale',
+    };
+    await request(app)
+      .get('/api/v1/movies/3')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, expected);
+  });
+
+  it('should return 200 when movie was deleted', async () => {
+    await request(app)
+      .delete('/api/v1/movies/4')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, { message: 'Deleted' });
+    await request(app)
+      .get('/api/v1/movies/4')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, { message: 'Not found' });
+  });
+
 });
+
+
+
+
+
